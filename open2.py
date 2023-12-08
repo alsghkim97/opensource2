a97cb975cb4b881a813775ca09dd2e6b3e9bfade
Kim Minho <alsghkim97@naver.com>
2023-12-08 22:42:45 +0900

    hint func add
      hint 함수 선언
             -힌트: (정답-10) ~ (정답+10)
             -힌트버튼 클릭시 팝업창
             -hintButton과 연동

--- open2.py
+++ open2.py
@@ -51,6 +51,19 @@ def guessing():
     resultLabel2["text"]=msg
     guessField.delete(0,5)
 
+def hint():
+    
+    hint1=answer-10
+    if(hint1<0):
+        hint1=0
+     
+    hint2=answer+10
+    if(hint2>100):
+        hint2=100
+        
+    messagebox.showinfo("힌트",f"{hint1} ~ {hint2} 사이의 숫자")
+#힌트 함수
+#팝업창으로 띄우게하기
 
 def on_entry_click(event):
     if guessField.get() == "1에서 100까지의 숫자중 하나를 입력하세요":
@@ -74,7 +87,7 @@ tryButton.place(x=377,y=300)
 resetButton = Button(f1,text="초기화",fg="red",bg="white")
 resetButton.place(x=413,y=300)
 
-hintButton = Button(f1,text="힌트",fg="blue",bg="white")
+hintButton = Button(f1,text="힌트",fg="blue",bg="white",command=hint)
 hintButton.place(x=461,y=300)
 
 histogram_button = tk.Button(window, text="통계",fg="orange",bg="white")
