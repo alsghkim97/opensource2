0e29051c6a02d74acafbb3d574b19f53cb765570
Kim Minho <alsghkim97@naver.com>
2023-12-08 22:28:44 +0900

    canvas1 add
      학번/이름 캔버스 추가
             -바탕:darkblue font: italic bold
             -width=450,height=150

--- open2.py
+++ open2.py
@@ -7,8 +7,13 @@ window.geometry('600x700')
 window.configure(bg="white")
 window.title("숫자를 맟줘보세요")  #실행창 제목
 
+f1=Frame(window,relief="solid")
+canvas1 = Canvas(f1,width=450,height=150, bg="#afeeee")
+canvas1.create_text(225,75,fill="darkblue",font="Times 30 italic bold",
+                   text=" 20160814\n  김민호") 
+
+canvas1.place(x=75,y=50) #학번 이름 칸
 
 
-
-
+f1.pack(fill="both",expand=True)
 window.mainloop()
