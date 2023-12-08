1e4a5e36c51985fe31f5aa5d2e5c2be115dbb1ed
Kim Minho <alsghkim97@naver.com>
2023-12-08 22:26:38 +0900

    window create
     파일 생성 실행창 제목/크기 설정

---
+++ open2.py
@@ -0,0 +1,14 @@
+from tkinter import*
+import tkinter as tk
+
+window = Tk()
+window.geometry('600x700')
+
+window.configure(bg="white")
+window.title("숫자를 맟줘보세요")  #실행창 제목
+
+
+
+
+
+window.mainloop()
