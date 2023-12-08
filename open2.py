34b40a151a17e929bdee434a21df84ce2c546c0b
Kim Minho <alsghkim97@naver.com>
2023-12-08 22:36:23 +0900

    hintButton add
          힌트버튼 추가
             -바탕색:white / 글자색:blue
             (함수연동 필요)

--- open2.py
+++ open2.py
@@ -32,8 +32,12 @@ guessField.insert(0, "1에서 100까지의 숫자중 하나를 입력하세요")
 
 tryButton = Button(f1,text="시도",fg="green",bg="white")
 tryButton.place(x=377,y=300)
+
 resetButton = Button(f1,text="초기화",fg="red",bg="white")
 resetButton.place(x=413,y=300)
+
+hintButton = Button(f1,text="힌트",fg="blue",bg="white")
+hintButton.place(x=461,y=300)
 
 
 
